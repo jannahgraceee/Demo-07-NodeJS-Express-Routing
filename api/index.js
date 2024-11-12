f33cbc693e5704650f8a7e406dc1d538cd49b6b3
jannahgraceee <125098286+jannahgraceee@users.noreply.github.com>
2024-11-12 04:20:56 +0000

    added homepage

--- api/index.js
+++ api/index.js
@@ -9,6 +9,11 @@ app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
+//Home Page
+app.get('/', (request, response) => {
+  response.send('<h1>API Homepage</h1>');
+});
+
 //Customers
 const customerRouter = require ('./routes/customer');
 app.use('/customer', customerRouter);
